858e6c2a358b36858b34b538f06ddbef5a1b9ea0
rzawar <zawar.rohit@gmail.com>
2014-11-13 13:32:43 -0500

    Changes made for the archived data

--- extensions/sep.py
+++ extensions/sep.py
@@ -9,4 +9,26 @@ for id,label in labels.iteritems():
     labels[id] = HTMLParser().unescape(label)
 
 def label(doc):
-    return labels.get(doc, doc)
+    
+    doc = doc.split('-',1);
+    print doc
+    sem = str(doc[0])
+    match = re.match(r"([a-z]+)([0-9]+)",sem, re.I)
+    if match:
+    	items = match.groups()
+	print items
+    year = items[1]
+    sem = items[0]
+    if(sem == 'spr'):
+	sem = "Spring"
+    if(sem == 'win'):
+        sem = "Winter"
+    if(sem == 'sum'):
+        sem = "Summer"
+    if(sem == 'fall'):
+        sem = "Fall"
+    
+    topic = doc[1] 
+    topic = str(topic.split('.')[0])
+    topic = labels.get(topic, topic)
+    return topic+' ['+sem+' '+year+'] '
