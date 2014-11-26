d46f0af95448eea57e6faa1bc573450f3fcbc706
Jaimie Murdock <jaimie.murdock@gmail.com>
2014-11-25 23:59:41 -0500

    two methods for doing http->https redirect

--- bottle_ssl.py
+++ bottle_ssl.py
@@ -1,4 +1,17 @@
-from bottle import ServerAdapter
+from bottle import install, redirect, request, ServerAdapter
+
+def redirect_http_to_https(callback):
+    '''Bottle plugin that redirects all http requests to https'''
+
+    def wrapper(*args, **kwargs):
+        scheme = request.urlparts[0]
+        if scheme == 'http':
+            # request is http; redirect to https
+            redirect(request.url.replace('http', 'https', 1))
+        else:
+            # request is already https; okay to proceed
+            return callback(*args, **kwargs)
+    return wrapper
 
 # copied from bottle. Only changes are to import ssl and wrap the socket
 class SSLWSGIRefServer(ServerAdapter):
