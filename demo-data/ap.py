84f1f2f5b3571cd36722a20057126917bf7b3174
Jaimie Murdock <jaimie.murdock@gmail.com>
2015-03-07 00:00:45 -0500

    adding helpful tip messages and testing for dependencies

--- demo-data/ap.py
+++ demo-data/ap.py
@@ -8,23 +8,48 @@ This example can be used to help guide creation of other corpora for the
 Topic Explorer, in conjunction with the ap.ini file.
 """
 import numpy as np
-import os, os.path
+import os, os.path, shutil
 from StringIO import StringIO
 import tarfile
 import xml.etree.ElementTree as ET
 import sys
 
-import wget
+
+print "Checking for wget, unidecode and nltk packages"
+def install_and_import(package):
+    import importlib
+    try:
+        importlib.import_module(package)
+    except ImportError:
+        import pip
+        pip.main(['install', '--user', package])
+    finally:
+        globals()[package] = importlib.import_module(package)
+
+
+install_and_import('wget')
+install_and_import('unidecode')
+install_and_import('nltk')
+
+print "Checking for nltk stopwords and punkt tokenizer"
+nltk.download('stopwords')
+nltk.download('punkt')
+
+
 
 from vsm import *
 from vsm.extensions.corpusbuilders.util import apply_stoplist, word_tokenize
 from vsm.viewer.wrappers import doc_label_name
 
 # parse the pseudo-xml document into a python-native dict
-print "Downloading ap.tgz"
-filename = wget.download('http://www.cs.princeton.edu/~blei/lda-c/ap.tgz')
+if not os.path.exists('ap.tgz'):
+    print "Downloading demo-data/ap.tgz"
+    filename = wget.download('http://www.cs.princeton.edu/~blei/lda-c/ap.tgz')
+else:
+    print "Processing demo-data/ap.tgz"
+    filename = 'ap.tgz'
 
-print "parsing ap/ap.txt"
+print "\nParsing ap/ap.txt"
 with tarfile.open(filename, 'r') as apfile:
     member = apfile.getmember('ap/ap.txt')
     ap89_f = apfile.extractfile(member)
@@ -43,13 +68,22 @@ for doc in docs:
     text = doc.find('TEXT').text.strip().replace('&#038;', '&')
     corpus[docno] = text
 
+# check if directory already exists
 if os.path.exists("ap"):
-    print "ap folder already exists!"
-    sys.exit(74)
-else:
-    os.mkdir("ap")
+    print "Folder 'ap' already exists!"
+    remove = None
+    while remove is None or not (remove.startswith('y') or remove.startwith('n')):
+        remove = raw_input("Remove? [Y/n] ")
+        remove = remove.lower()
+        if remove == '' or remove.startswith('y'):
+            shutil.rmtree("ap")
+        elif remove.startswith('n'):
+            sys.exit(74)
 
-
+# extracting to individual files
+os.mkdir("ap")
+print "Extracting documents to demo-data/ap folder"
 for doc,text in corpus.items():
     with open('ap/'+doc, 'w') as outfile:
         outfile.write(text)
+
--- demo-data/get-data.sh
+++ demo-data/get-data.sh
@@ -1,4 +1,8 @@
 #!/bin/sh -e
 python ap.py
 cd ..
-python train.py -k 10 20 30 40 50 60 70 80 90 100 --iter 20 demo-data/ap
+echo "Training ap corpus with command:"
+echo "python train.py -k 10 20 30 40 50 60 70 8090 100 --iter 20 -p 2 demo-data/ap"
+echo ""
+echo "Run 'python train.py -h' for more details on training your own corpus"
+python train.py -k 10 20 30 40 50 60 70 80 90 100 --iter 20 -p 2 demo-data/ap
