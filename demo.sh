ae0f15a1a3bdbe40a5759a77ace83fbe4364ccca
Jaimie Murdock <jaimie.murdock@gmail.com>
2015-04-21 19:14:04 -0400

    updating demo file

--- demo.sh
+++ demo.sh
@@ -3,11 +3,11 @@ cd demo-data
 python ap.py
 cd ..
 echo "Training ap corpus with command:"
-echo "vsm train -k 10 20 30 40 50 60 70 80 90 100 --iter 20 -p 2 demo-data/ap"
+echo "vsm init demo-data/ap ap.ini"
+echo "vsm train ap.ini -k 10 20 30 40 50 --iter 20 --context-type document -p 2"
 echo ""
 echo "Run 'vsm train -h' for more details on training your own corpus"
 echo ""
 vsm init demo-data/ap ap.ini
-vsm train -k 10 20 --iter 20 -p 2 ap.ini 
-#vsm train -k 10 20 30 40 50 60 70 80 90 100 --iter 20 -p 2 ap.ini 
+vsm train ap.ini -k 10 20 30 40 50 --iter 20 --context-type document -p 2 
 
