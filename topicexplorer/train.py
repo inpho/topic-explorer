516848a4d14436978e1fc4cc6e07a2051371ae08
Jaimie Murdock <jaimie.murdock@gmail.com>
2015-05-26 11:34:25 -0400

    adding continue training functionality. see #74

--- topicexplorer/train.py
+++ topicexplorer/train.py
@@ -1,13 +1,16 @@
 from ConfigParser import RawConfigParser as ConfigWriter
 from ConfigParser import SafeConfigParser as ConfigParser
+from ConfigParser import NoOptionError
 import multiprocessing
 import os.path
 
 from vsm.corpus import Corpus
 from vsm.model.lda import LDA
 
-def build_models(corpus, corpus_filename, model_path, context_type, krange, n_iterations=200,
-                 n_proc=2, seed=None):
+from topicexplorer.lib.util import bool_prompt
+
+def build_models(corpus, corpus_filename, model_path, context_type, krange, 
+                 n_iterations=200, n_proc=2, seed=None):
 
     basefilename = os.path.basename(corpus_filename).replace('.npz','')
     basefilename += "-LDA-K%s-%s-%d.npz" % ('{0}', context_type, n_iterations)
@@ -24,14 +27,38 @@ def build_models(corpus, corpus_filename, model_path, context_type, krange, n_it
     for k in krange:
         print "Training model for k={0} Topics with {1} Processes"\
             .format(k, n_proc)
-        m = LDA(corpus, context_type, K=k, multiprocessing=True, n_proc=n_proc, seed_or_seeds=seeds)
+        m = LDA(corpus, context_type, K=k, multiprocessing=True, n_proc=n_proc,
+                seed_or_seeds=seeds)
         m.train(n_iterations=n_iterations)
         m.save(basefilename.format(k))
 
     return basefilename
 
+def continue_training(model_pattern, krange, n_iterations=200):
+    for k in krange:
+        m = LDA.load(model_pattern.format(k))
+
+        print "Continue training model for k={0} Topics".format(k)
+        m.train(n_iterations=n_iterations)
+
+        # save new file
+        basefilename = model_pattern.replace(
+            "-{orig}.npz".format(orig=(m.iteration - n_iterations)),
+            "-{new}.npz".format(new=m.iteration))
+        m.save(basefilename.format(k))
+
+    return basefilename
+
 def main(args):
+    config = ConfigParser()
+    config.read(args.config_file)
+    corpus_filename = config.get("main", "corpus_file")
+    model_path = config.get("main", "path")
+
     if args.k is None:
+        if config.get("main", "topics"):
+            args.k = eval(config.get("main", "topics"))
+
         while args.k is None:
             ks = raw_input("Number of Topics [Default '20 40 60 80']: ")
             try:
@@ -39,12 +66,14 @@ def main(args):
                     args.k = [int(n) for n in ks.split()]
                 elif not ks.strip():
                     args.k = range(20,100,20) 
+
+                if args.k:
+                    print "\nTIP: number of topics can be specified with argument '-k N N N ...':"
+                    print "         vsm train %s -k %s\n" %\
+                             (args.config_file, ' '.join(map(str, args.k)))
             except ValueError:
                 print "Enter valid integers, separated by spaces!"
         
-        print "\nTIP: number of topics can be specified with argument '-k N N N ...':"
-        print "         vsm train %s -k %s\n" %\
-            (args.config_file, ' '.join(map(str, args.k)))
     
     if args.iter is None:
         while args.iter is None:
@@ -60,42 +89,50 @@ def main(args):
         print "\nTIP: number of training iterations can be specified with argument '--iter N':"
         print "         vsm train --iter %d %s\n" % (args.iter, args.config_file)
 
-    if args.processes < 0:
-        args.processes = multiprocessing.cpu_count() + args.processes
 
-    config = ConfigParser()
-    config.read(args.config_file)
-    corpus_filename = config.get("main", "corpus_file")
-    model_path = config.get("main", "path")
+    try:
+        model_pattern = config.get("main", "model_pattern")
+    except NoOptionError:
+        model_pattern = None
 
-    corpus = Corpus.load(corpus_filename)
-    # TODO: Prompt for context_type
-    ctxs = corpus.context_types
-    ctxs = sorted(ctxs, key=lambda ctx: len(corpus.view_contexts(ctx)))
-    if args.context_type not in ctxs:
-        while args.context_type not in ctxs:
-            contexts = ctxs[:]
-            contexts[0] = contexts[0].upper()
-            contexts = '/'.join(contexts)
-            args.context_type = raw_input("Select a context type [%s] : " % contexts)
-            if args.context_type.strip() == '':
-                args.context_type = ctxs[0]
-            if args.context_type == ctxs[0].upper():
-                args.context_type = ctxs[0]
+    if model_pattern is not None and\
+        bool_prompt("Existing model found. Continue training?", default=True):
+        # continue training
+        model_pattern = continue_training(model_pattern, args.k, args.iter)
 
-        print "\nTIP: context type can be specified with argument '--context-type TYPE':"
-        print "         vsm train --context-type %s %s\n" % (args.context_type, args.config_file)
-
-
-    print "\nTIP: This configuration can be automated as:"
-    print "         vsm train %s --iter %d --context-type %s -k %s\n" %\
-        (args.config_file, args.iter, args.context_type, 
-            ' '.join(map(str, args.k)))
+    else:
+        # build a new model
+        corpus = Corpus.load(corpus_filename)
     
-    model_pattern = build_models(corpus, corpus_filename, model_path, 
-                                 args.context_type, args.k,
-                                 n_iterations=args.iter,
-                                 n_proc=args.processes, seed=args.seed)
+        ctxs = corpus.context_types
+        ctxs = sorted(ctxs, key=lambda ctx: len(corpus.view_contexts(ctx)))
+        if args.context_type not in ctxs:
+            while args.context_type not in ctxs:
+                contexts = ctxs[:]
+                contexts[0] = contexts[0].upper()
+                contexts = '/'.join(contexts)
+                args.context_type = raw_input("Select a context type [%s] : " % contexts)
+                if args.context_type.strip() == '':
+                    args.context_type = ctxs[0]
+                if args.context_type == ctxs[0].upper():
+                    args.context_type = ctxs[0]
+    
+            print "\nTIP: context type can be specified with argument '--context-type TYPE':"
+            print "         vsm train --context-type %s %s\n" % (args.context_type, args.config_file)
+    
+    
+        print "\nTIP: This configuration can be automated as:"
+        print "         vsm train %s --iter %d --context-type %s -k %s\n" %\
+            (args.config_file, args.iter, args.context_type, 
+                ' '.join(map(str, args.k)))
+    
+        if args.processes < 0:
+            args.processes = multiprocessing.cpu_count() + args.processes
+    
+        model_pattern = build_models(corpus, corpus_filename, model_path, 
+                                     args.context_type, args.k,
+                                     n_iterations=args.iter,
+                                     n_proc=args.processes, seed=args.seed)
 
     config.set("main", "model_pattern", model_pattern)
     config.set("main", "context_type", args.context_type)
