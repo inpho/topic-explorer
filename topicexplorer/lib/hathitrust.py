2d4b125fce862d458e1de60e9cde06411dd7c985
Jaimie Murdock <jaimie.murdock@gmail.com>
2015-07-07 10:32:24 -0400

    adding marc reading support

--- topicexplorer/lib/hathitrust.py
+++ topicexplorer/lib/hathitrust.py
@@ -7,9 +7,11 @@ import os.path
 from StringIO import StringIO ## used to stream http response into zipfile.
 import sys
 from time import sleep
-from urllib2 import urlopen
+from urllib2 import urlopen, HTTPError
 from urllib import quote_plus, urlencode
+import xml.etree.ElementTree as ET
 from zipfile import ZipFile  ## used to decompress requested zip archives.
+
 
 from topicexplorer.lib.util import *
 
@@ -23,7 +25,7 @@ def metadata(id, sleep_time=1):
         data = json.load(urlopen(solr))
         print id
         return data['response']['docs'][0]
-    except (ValueError,IndexError):
+    except (ValueError,IndexError, HTTPError):
         print "No result found for " + id 
         return dict()
 
@@ -33,7 +35,52 @@ def get_metadata(folder):
     data = dict(data)
     with open(os.path.join(folder, '../metadata.json'),'wb') as outfile:
         json.dump(data, outfile)
+
+"""
+MARC CODE HANDLING
+"""
+def parse_marc(raw):
+    # lazy workaround
+    raw = raw.replace(' xmlns', ' xmlnamespace')
+    ET.register_namespace('', 'http://www.loc.gov/MARC21/slim')
+    return ET.fromstring(raw)
+
+def get_marc_value(xml, tag, code):
+    xpath = "{marc}datafield[@tag='{tag}']/{marc}subfield[@code='{code}']".format(
+                tag=tag, code=code, marc='')# marc="{http://www.loc.gov/MARC21/slim}")
+    results = xml.findall(xpath)
+    return results[0].text if results else None
+
+def get_lccn_from_marc(xml):
+    return get_marc_value(xml, '010', 'a')
+
+def get_title_from_marc(xml):
+    return get_marc_value(xml, '245', 'a')
+
+def get_volume_from_marc(xml):
+    return get_marc_value(xml, '974', 'c')
+
+def get_lcc_from_marc(xml):
+    # MARC tag 050a/b or 991h/i
+    lcc = list()
+    val = get_marc_value(xml, '050', 'a')
+    if val:
+        lcc.append(val)
     
+    val = get_marc_value(xml, '050', 'b')
+    if val:
+        lcc[-1] += val
+
+    val = get_marc_value(xml, '991', 'h')
+    if val:
+        lcc.append(val)
+
+    val = get_marc_value(xml, '991', 'i')
+    if val:
+        lcc[-1] += val
+
+    return ";".join(lcc)
+
 
 
 """
