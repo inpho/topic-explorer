f2ce17ffbe6c0d73f113372379cbc10f9cbfa662
Jaimie Murdock <jaimie.murdock@gmail.com>
2016-01-26 21:50:27 -0500

    Adding output checks for PyPI push

--- register.py
+++ register.py
@@ -1,4 +1,5 @@
 import os
+from subprocess import check_output, CalledProcessError
 import sys
 from time import sleep
 
@@ -25,26 +26,61 @@ commits_ahead =list(repo.iter_commits(
 if not commits_ahead and not commits_behind:
     pass
 elif commits_ahead and not commits_behind:
-    print "Git branch '{}' ahead of origin. Pushing changes.".format(branch.name)
+    print "Git branch '{}' ahead of origin. Pushing changes.\n".format(branch.name)
     repo.remotes.origin.push(branch.name)
 else:
-    print "Git branch '{}' out of sync. Aborting release.".format(branch.name)
+    print "Git branch '{}' out of sync. Aborting release.\n".format(branch.name)
     sys.exit(1)
 
 current_commit = repo.head.commit.hexsha
 
 ### TODO: Automagically increment version number.
 
-if __version__ not in repo.tags:
-    print "Creating tag for release."
-    repo.create_tag(__version__)
+### Check version numbers are not already online
+if __version__ in repo.tags and current_commit != repo.tags[__version__]:
+    print "Local version tag already exists: ", __version__
+    print "Increment the version number and run release again.\n"
+    sys.exit(1)
+elif (__version__ in repo.remotes.origin.repo.tags and 
+      current_commit != repo.remotes.origin.repo.tags[__version__]):
+    print "GitHub version tag already exists: ", __version__
+    print "Increment the version number and run release again.\n"
+    sys.exit(1)
+
 
 ### TRAVIS-CI CHECKS ###
-t = TravisPy.github_auth(open('.travis.key').read().strip())
-print "Waitng for Travis-CI",
+try:
+    t = TravisPy.github_auth(open('.travis.key').read().strip())
+except IOError as e:
+    if e.errno == 2:
+        print ".travis.key file required to hold GitHub Auth Token."
+        url = "https://github.com/settings/tokens/new"
+        url += "?scopes=repo_deployment,repo:status,write:repo_hook,read:org,user:email"
+        url += "&description=travis%20ci%20token"
+        # TODO: Prompt to open browser or automate token grab
+        print url + '\n'
+        sys.exit(1)
+    else:
+        raise e
+except TravisError:
+    if not open('.travis.key').read().strip():
+        print ".travis.key file is empty. Fill with a GitHub Auth Token from:"
+        url = "https://github.com/settings/tokens/new"
+        url += "?scopes=repo_deployment,repo:status,write:repo_hook,read:org,user:email"
+        url += "&description=travis%20ci%20token"
+        # TODO: Prompt to open browser or automate token grab
+        print url
+    else:
+        print ".travis.key file detected, but there was an error communicating with Travis."
+        print "Check your GitHub Auth Token or check the Travis status page at:"
+        print "https://www.traviscistatus.com/"
+    print " "
+    sys.exit(1)
+        
+print "Waitng for Travis-CI .",
 for attempt in range(900):
     try:
-        branch = t.branch(__version__, 'inpho/topic-explorer')
+        branch = t.branch(repo.active_branch, 'inpho/topic-explorer')
 
         if current_commit != branch.commit.sha:
             raise RuntimeError("Need to wait for commit to sync.")
@@ -55,28 +91,37 @@ for attempt in range(900):
         print ".",
         sleep(10)
 else:
-    print "Travis build not complete. Aborting release."
+    print "Travis build not complete. Aborting release.\n"
     sys.exit(1)
 
 if branch.finished and branch.passed:
-    print "Travis build of release {} passed!".format(__version__) 
+    print "Travis build of release {} passed!\n".format(__version__) 
 else:
-    print "Travis build of release {} failed. Aborting release.".format(__version__)
+    print "Travis build of release {} failed. Aborting release.\n".format(__version__)
+    sys.exit(1)
 
 
 ### Convert documentation for PyPI ###
-"""
 pypandoc.convert('README.md', 'rst', outputfile='README.txt')
-if sys.argv[-1] == 'test':
-    os.system("python setup.py register -r pypitest")
-else:
-    os.system("python setup.py register")
-    os.system("python setup.py sdist upload")
-    os.system("python setup.py bdist_egg upload")
-os.remove('README.txt')
-"""
+try:
+    if sys.argv[-1] == 'test':
+        check_output("python setup.py register -r pypitest", shell=True)
+    else:
+        check_output("python setup.py register", shell=True)
+        check_output("python setup.py sdist upload", shell=True)
+        check_output("python setup.py bdist_egg upload", shell=True)
+except CalledProcessError as e:
+    print "\nFailed to register and upload the package to PyPI.\n"
+    sys.exit(1)
+finally:
+    os.remove('README.txt')
 
-print repo.remotes.origin.refs
-if repo.remotes.origin.refs.get(__version__):
-    print "Pushing tag for release to GitHub."
+if __version__ not in repo.tags:
+    print "Creating tag for release.\n"
+    repo.create_tag(__version__)
+
+if __version__ not in repo.remotes.origin.repo.tags:
+    print "Pushing tag for release to GitHub.\n"
     repo.remotes.origin.push(__version__)
+
+print "Release complete.\n"
