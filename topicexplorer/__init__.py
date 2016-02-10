d3210d52e640283e4226fffa729beaafcef58aa9
Jaimie Murdock <jaimie.murdock@gmail.com>
2016-02-09 21:10:09 -0500

    version hacking

--- topicexplorer/__init__.py
+++ topicexplorer/__init__.py
@@ -1,8 +1,1 @@
-from __version__ import __version__, __pretty_version__ as __pv__
-
-@property
-def __pretty_version__():
-    if __pretty_version__ is None:
-        __pretty_version__ = __version__
-    return __pretty_version__
-
+from __version__ import __version__, __pretty_version__
--- topicexplorer/__version__.py
+++ topicexplorer/__version__.py
@@ -1,18 +1,25 @@
 """Version information."""
+import types
+import sys
+class _VersionModule(types.ModuleType):
+    @property
+    def __version__(self):
+        return "1.0b47"
 
-@property
-def __pretty_version__():
-    from topicexplorer.update import get_dist
-    from pip.utils import dist_is_editable
-    dist = get_dist('topicexplorer')
-    __pretty_version__ = None
-    if dist_is_editable(dist):
-        import subprocess
-        __pretty_version__ = subprocess.check_output(
-            'git describe --long --tags --always --dirty',
-            cwd=dist.location, shell=True)
-    return __pretty_version__
+    @property
+    def __pretty_version__(self):
+        from topicexplorer.update import get_dist
+        from pip.utils import dist_is_editable
+        dist = get_dist('topicexplorer')
+        __pv__ = None
+        if dist_is_editable(dist):
+            import subprocess
+            __pv__ = subprocess.check_output(
+                'git describe --long --tags --always --dirty',
+                cwd=dist.location, shell=True)
+        return __pv__ or self.__version__
 
-# The following line *must* be the last in the module, exactly as formatted:
-# See http://stackoverflow.com/a/17626524
-__version__ = "1.0b47"
+    # The following line *must* be the last in the module, exactly as formatted:
+    # See http://stackoverflow.com/a/17626524
+
+sys.modules[__name__] = _VersionModule("__version__")
