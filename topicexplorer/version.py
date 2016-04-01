4f2339fa38421aed1e1275bbce783a5d44a314e4
Jaimie Murdock <jaimie.murdock@gmail.com>
2016-04-01 11:16:23 -0400

    adding raw_corpus safety check

--- topicexplorer/version.py
+++ topicexplorer/version.py
@@ -5,7 +5,7 @@ import sys
 class _VersionModule(types.ModuleType):
     @property
     def __version__(self):
-        return "1.0b66"
+        return "1.0b67"
 
     @property
     def __pretty_version__(self):
