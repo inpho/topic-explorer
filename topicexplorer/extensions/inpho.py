62952e869f2245a6bd0f9e6989cf733d79b24474
Jaimie Murdock <jaimie.murdock@gmail.com>
2016-04-22 00:20:43 -0400

    adding better inpho tokenizer

--- topicexplorer/extensions/inpho.py
+++ topicexplorer/extensions/inpho.py
@@ -9,13 +9,14 @@ from sqlalchemy.sql import func
 def _inpho_token_generator(document):
     if PUNC_TABLE.get(ord('-')):
         del PUNC_TABLE[ord('-')]
+    PUNC_TABLE[ord('\n')] = ord(' ')
     
     rest = document.lower()
     rest = rehyph(rest)
     rest = strip_punc_word(rest)
     query = Session.query(Searchpattern)
 
-    MIN_LEN = 6
+    MIN_LEN = 6 
     short_patterns = Session.query(Searchpattern.searchpattern)
     short_patterns = short_patterns.filter(func.length(Searchpattern.searchpattern) < MIN_LEN)
     short_patterns = short_patterns.distinct().all()
@@ -47,7 +48,8 @@ def _inpho_token_generator(document):
             # check if multi-phrase starts match in the rest of the phrase.
             if u' ' in p.searchpattern:
                 first_pattern_word, longpattern = p.searchpattern.split(u' ',  1)
-                if first == first_pattern_word and rest.startswith(u' ' + longpattern + u' '):
+                if first == first_pattern_word and (rest == longpattern 
+			or rest.startswith(longpattern + u' ')):
                     yield u"inpho:{}".format(p.entity.ID)
             elif first == p.searchpattern:
                 yield u"inpho:{}".format(p.entity.ID)
