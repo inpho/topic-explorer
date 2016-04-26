5e64fe99472f05b57d6d747bcd91dc2550b2f40f
Jaimie Murdock <jaimie.murdock@gmail.com>
2016-04-26 11:02:26 -0400

    updating version for ldasentence fixes upstream

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ install_requires = [
         'bottle>=0.12', 
         'brewer2mpl>=1.4',
         'pystache>=0.5.4',
-        'vsm==0.4.0a24',
+        'vsm==0.4.0a25',
         'wget',
         'unidecode',
         'pdfminer',
