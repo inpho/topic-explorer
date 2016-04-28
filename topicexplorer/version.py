dd74a5cbe665c8d339c02ae85c22547e1b015375
Jaimie Murdock <jaimie.murdock@gmail.com>
2016-04-28 12:54:56 -0400

    version bump for downstream changes

--- topicexplorer/version.py
+++ topicexplorer/version.py
@@ -5,7 +5,7 @@ import sys
 class _VersionModule(types.ModuleType):
     @property
     def __version__(self):
-        return "1.0b86"
+        return "1.0b87"
 
     @property
     def __pretty_version__(self):
