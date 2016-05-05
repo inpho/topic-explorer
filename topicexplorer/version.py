cc667b877de457465883fb9934356a6b1fdc8b56
Jaimie Murdock <jaimie.murdock@gmail.com>
2016-05-05 13:38:49 -0400

    version bump for KeyError hotfix

--- topicexplorer/version.py
+++ topicexplorer/version.py
@@ -5,7 +5,7 @@ import sys
 class _VersionModule(types.ModuleType):
     @property
     def __version__(self):
-        return "1.0b88"
+        return "1.0b89"
 
     @property
     def __pretty_version__(self):
