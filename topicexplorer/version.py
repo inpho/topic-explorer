3582463d181da03c50ec9d70db460f1f95a45881
Jaimie Murdock <jaimie.murdock@gmail.com>
2016-05-13 03:18:39 -0400

    version bump for bibtex merge

--- topicexplorer/version.py
+++ topicexplorer/version.py
@@ -5,7 +5,7 @@ import sys
 class _VersionModule(types.ModuleType):
     @property
     def __version__(self):
-        return "1.0b91"
+        return "1.0b92"
 
     @property
     def __pretty_version__(self):
