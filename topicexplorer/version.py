a58b0f863074670a3c34a33e7f564688f9026c11
Jaimie Murdock <jaimie.murdock@gmail.com>
2016-06-21 18:27:31 -0400

    version bump

--- topicexplorer/version.py
+++ topicexplorer/version.py
@@ -5,7 +5,7 @@ import sys
 class _VersionModule(types.ModuleType):
     @property
     def __version__(self):
-        return "1.0b97"
+        return "1.0b98"
 
     @property
     def __pretty_version__(self):
