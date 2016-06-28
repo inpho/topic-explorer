5fa593cb8fdf250db2dabd54b22787edf15cd4a3
Jaimie Murdock <jaimie.murdock@gmail.com>
2016-06-28 11:12:12 -0400

    adding chinese merge branch to released version

--- topicexplorer/version.py
+++ topicexplorer/version.py
@@ -5,7 +5,7 @@ import sys
 class _VersionModule(types.ModuleType):
     @property
     def __version__(self):
-        return "1.0b99"
+        return "1.0b100"
 
     @property
     def __pretty_version__(self):
