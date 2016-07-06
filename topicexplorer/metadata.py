ec07dd7edf88dee1ed0bf3568d3e41ccba758d7a
Jaimie Murdock <jaimie.murdock@gmail.com>
2016-07-06 00:52:53 -0400

    adding csv import

--- topicexplorer/metadata.py
+++ topicexplorer/metadata.py
@@ -1,6 +1,7 @@
 from ConfigParser import RawConfigParser as ConfigParser
 
 from codecs import open 
+import csv
 from unidecode import unidecode
 from topicexplorer.lib.util import isint, is_valid_configfile, bool_prompt
 
@@ -12,8 +13,13 @@ def parse_metadata_from_csvfile(filename):
     Returns a dictionary of dictionaries where each key is the label,
     and each value is a dictionary of field values.
     """
-    pass
+    with open(filename) as csvfile:
+        reader = csv.DictReader(csvfile)
+        metadata = dict()
+        for row in reader:
+            metadata[row['article_label']] = row
 
+    return metadata
 
 def extract_metadata(corpus, ctx_type, filename, format='csv'):
     """
@@ -35,7 +41,7 @@ def extract_labels(corpus, ctx_type, filename):
             outfile.write(label + '\n')
 
 
-def add_metadata(corpus, ctx_type, new_metadata):
+def add_metadata(corpus, ctx_type, new_metadata, force=False):
     import vsm.corpus
     
     # get existing metadata
@@ -48,25 +54,34 @@ def add_metadata(corpus, ctx_type, new_metadata):
     # for each entry - which is the desired error to throw.
     label_name = doc_label_name(ctx_type)
     labels = md[label_name]
-    new_data = [new_metadata[id] for id in labels]
+    new_data = [new_metadata.get(id, dict()) for id in labels]
 
     # look for new fields
     new_fields = set()
     for vals in new_metadata.values():
-        new_fields.add(vals.keys())
+        new_fields.update(vals.keys())
 
     # differentiate new and updated fields
     updated_fields = new_fields.intersection(fields)
+    updated_fields.remove(label_name)
     new_fields = new_fields.difference(fields)
+    if None in new_fields:
+        new_fields.remove(None)
 
     # process new fields
     for field in new_fields:
-        data = [d[field] for d in new_data] #TODO: sort field by _label
+        if force:
+            data = [d.get(field, '') for d in new_data]
+        else:
+            data = [d[field] for d in new_data]
         corpus = vsm.corpus.add_metadata(corpus, ctx_type, field, data)
 
     # process existing fields
     for field in updated_fields:
-        data = [d[field] for d in new_data] #TODO: sort field by _label
+        if force:
+            data = [d.get(field, '') for d in new_data]
+        else:
+            data = [d[field] for d in new_data]
         corpus.context_data[i][field] = data
 
     return corpus
@@ -85,8 +100,8 @@ def main(args):
     context_type = config.get('main', 'context_type')
 
     if args.add:
-        metadata = parse_metadata_from_csvfile(filename)
-        c = add_metadata(c, context_type, metadata)
+        metadata = parse_metadata_from_csvfile(args.add)
+        c = add_metadata(c, context_type, metadata, force=args.force)
         c.save(args.corpus_path)
     if args.list:
         extract_labels(c, context_type, args.list)
@@ -98,6 +113,8 @@ def populate_parser(parser):
     parser.add_argument("-e", "--extract", help="Extract metadata to file")
     parser.add_argument("-a", "--add", help="Add metadata from file")
     parser.add_argument("-l", "--list", help="List all labels")
+    parser.add_argument("-f", "--force", action='store_true', default=False,
+        help="Force insertion of blank metadata")
 
 
 if __name__ == '__main__':
