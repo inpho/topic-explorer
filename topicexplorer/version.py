1e8a515724027c41f5d49b05da6692448ac3a500
Jaimie Murdock <jaimie.murdock@gmail.com>
2016-07-08 14:28:01 -0400

    incrementing version number

--- topicexplorer/version.py
+++ topicexplorer/version.py
@@ -5,7 +5,7 @@ import sys
 class _VersionModule(types.ModuleType):
     @property
     def __version__(self):
-        return "1.0b103"
+        return "1.0b104"
 
     @property
     def __pretty_version__(self):
