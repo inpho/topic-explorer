6fab51fbade9a90ecf9145798024e90c8ef3fe9e
Jaimie Murdock <jaimie.murdock@gmail.com>
2016-07-31 02:13:05 -0400

    adding append mode to coverage scripts

--- coverage.sh
+++ coverage.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-CMD="coverage run --source=topicexplorer --omit=topicexplorer/lib,topicexplorer/extensions"
+CMD="coverage run -a --source=topicexplorer --omit=topicexplorer/lib,topicexplorer/extensions"
+
 rm -rf .coverage
+coverage debug sys
+
 $CMD topicexplorer/__main__.py version
 $CMD topicexplorer/__main__.py demo
 $CMD topicexplorer/__main__.py train ap.ini --rebuild -k 20 40 60 --iter 20 --context-type article
 $CMD topicexplorer/__main__.py update
 
 #TODO: Figure out test of launch
+coverage report
