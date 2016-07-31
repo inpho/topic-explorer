7865892271f3c0cb01a863617712eca81543fb5e
Jaimie Murdock <jaimie.murdock@gmail.com>
2016-07-31 02:58:14 -0400

    Update coverage.sh

--- coverage.sh
+++ coverage.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-CMD='coverage run -a --source topicexplorer --omit="topicexplorer/extensions/*.py,topicexplorer/lib/hathitrust.py"'
-
+#CMD='coverage run -a --source topicexplorer --omit="topicexplorer/extensions/*.py,topicexplorer/lib/hathitrust.py"'
+CMD="coverage run -a --source topicexplorer.init,topicexplorer.prep,topicexplorer.train,topicexplorer.server"
 rm -rf .coverage
 coverage debug sys
 
-$CMD topicexplorer/__main__.py version
-$CMD topicexplorer/__main__.py demo
-$CMD topicexplorer/__main__.py train ap.ini --rebuild -k 20 40 60 --iter 20 --context-type article
-$CMD topicexplorer/__main__.py update
+$CMD -m topicexplorer version
+$CMD -m topicexplorer.demo
+$CMD -m topicexplorer.train ap.ini --rebuild -k 20 40 60 --iter 20 --context-type article
+$CMD -m topicexplorer update
 
 #TODO: Figure out test of launch
 coverage report
