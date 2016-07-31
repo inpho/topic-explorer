490a5fdda5714119a049f81199f1ba03e0304923
Jaimie Murdock <jaimie.murdock@gmail.com>
2016-07-31 02:05:41 -0400

    adding coverage tests

--- coverage.sh
+++ coverage.sh
@@ -3,6 +3,7 @@ CMD="coverage run --source=topicexplorer --omit=topicexplorer/lib,topicexplorer/
 rm -rf .coverage
 $CMD topicexplorer/__main__.py version
 $CMD topicexplorer/__main__.py demo
-$CMD topicexplorer/__main__.py train ap.ini 
-$CMD topicexplorer/__main__.py launch
+$CMD topicexplorer/__main__.py train ap.ini --rebuild -k 20 40 60 --iter 20 --context-type article
 $CMD topicexplorer/__main__.py update
+
+#TODO: Figure out test of launch
