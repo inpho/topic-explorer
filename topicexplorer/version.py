bf456b1ca19b3fb1055ce49f1e7a8f2059b03c07
Jaimie Murdock <jaimie.murdock@gmail.com>
2016-09-15 14:39:12 -0400

    version bump for d3 update

--- topicexplorer/version.py
+++ topicexplorer/version.py
@@ -7,7 +7,7 @@ class _VersionModule(types.ModuleType):
 
     @property
     def __version__(self):
-        return "1.0b119"
+        return "1.0b120"
 
     @property
     def __pretty_version__(self):
