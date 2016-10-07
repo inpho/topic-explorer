1fecfddf3b899837a0a2db3973c93f347b43e250
Jaimie Murdock <jaimie.murdock@gmail.com>
2016-10-06 21:25:42 -0400

    adding config option for homepage link. closes #143

--- topicexplorer/server.py
+++ topicexplorer/server.py
@@ -395,7 +395,8 @@ class Application(Bottle):
                            'context_type': self.context_type,
                            'topic_range': self.topic_range,
                            'doc_title_format': kwargs.get('doc_title_format', '{0}'),
-                           'doc_url_format': kwargs.get('doc_url_format', '')}
+                           'doc_url_format': kwargs.get('doc_url_format', ''),
+                           'home_link': kwargs.get('home_link', '/')}
             return self.renderer.render(template, tmpl_params)
 
         @self.route('/<k:int>/')
@@ -582,6 +583,7 @@ def create_app(args):
         'topics': None,
         'cluster': None,
         'corpus_desc' : None,
+        'home_link' : '/',
         'lang': None})
     config.read(args.config)
 
@@ -609,6 +611,7 @@ def create_app(args):
     corpus_link = config.get('www', 'corpus_link')
     doc_title_format = config.get('www', 'doc_title_format')
     doc_url_format = config.get('www', 'doc_url_format')
+    home_link = config.get('www', 'home_link')
     label_module = config.get('main', 'label_module')
     corpus_path = config.get('main', 'raw_corpus')
     corpus_desc = config.get('main', 'corpus_desc')
@@ -629,7 +632,8 @@ def create_app(args):
                       doc_title_format=doc_title_format,
                       doc_url_format=doc_url_format,
                       cluster_path=cluster_path,
-                      corpus_desc=corpus_desc)
+                      corpus_desc=corpus_desc,
+                      home_link=home_link)
 
     """
     host, port = get_host_port(args) 
