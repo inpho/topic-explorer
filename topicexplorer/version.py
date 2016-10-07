7dcbdcf46e3dbfe7df7ac62638faa1aff3525b05
Jaimie Murdock <jaimie.murdock@gmail.com>
2016-10-06 22:03:22 -0400

    fixing icons

--- topicexplorer/version.py
+++ topicexplorer/version.py
@@ -7,7 +7,7 @@ class _VersionModule(types.ModuleType):
 
     @property
     def __version__(self):
-        return "1.0b131"
+        return "1.0b132"
 
     @property
     def __pretty_version__(self):
