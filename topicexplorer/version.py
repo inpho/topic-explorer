b6240a323cc62d6ceddbe563236e8b10fb7f9cde
Jaimie Murdock <jaimie.murdock@gmail.com>
2016-11-02 21:13:18 -0400

    version bump for wording changes

--- topicexplorer/version.py
+++ topicexplorer/version.py
@@ -7,7 +7,7 @@ class _VersionModule(types.ModuleType):
 
     @property
     def __version__(self):
-        return "1.0b138"
+        return "1.0b139"
 
     @property
     def __pretty_version__(self):
