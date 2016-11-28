17c61b7fb251d7ccadc6cd4a41e9e8c88c40938f
Jaimie Murdock <jaimie.murdock@gmail.com>
2016-11-28 12:55:06 -0500

    fixing the topics link in sidebar

--- topicexplorer/version.py
+++ topicexplorer/version.py
@@ -7,7 +7,7 @@ class _VersionModule(types.ModuleType):
 
     @property
     def __version__(self):
-        return "1.0b151"
+        return "1.0b152"
 
     @property
     def __pretty_version__(self):
