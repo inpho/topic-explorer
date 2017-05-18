274d5b1db31e4823ff4754d9c343a81ad7a940c9
Jaimie Murdock <jaimie.murdock@gmail.com>
2017-05-18 14:44:37 -0400

    merging in py3k branch. closes #200

--- topicexplorer/version.py
+++ topicexplorer/version.py
@@ -7,7 +7,7 @@ class _VersionModule(types.ModuleType):
 
     @property
     def __version__(self):
-        return "1.0b161"
+        return "1.0b162"
 
     @property
     def __pretty_version__(self):
