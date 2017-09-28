571ed5230eeaa658867d714f61c3f69300fdace3
Jaimie Murdock <jaimie.murdock@gmail.com>
2017-09-28 15:00:29 -0400

    updating coverage of topicexplorer.server

--- coverage.sh
+++ coverage.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 #CMD='coverage run -a --source topicexplorer --omit="topicexplorer/extensions/*.py,topicexplorer/lib/hathitrust.py"'
 CMD="coverage run -a --source topicexplorer.init,topicexplorer.prep,topicexplorer.train,topicexplorer.server"
-rm -rf .coverage
+rm -rf .coverage ap
 coverage debug sys
 
 EXIT=0
 $CMD -m topicexplorer version
 EXIT=$(($EXIT+$?))
-#echo "testing demo"
-#$CMD -m topicexplorer.demo --no-launch
+$CMD -m topicexplorer.demo --no-launch
 EXIT=$(($EXIT+$?))
 
-trap 'kill -INT $DEMO_PID' TERM INT
+# Special thanks on the `trap` semantics to:
+# http://veithen.github.io/2014/11/16/sigterm-propagation.html
+set -m # Reactivate monitor mode!
 $CMD -m topicexplorer.server -p 8000 --no-browser ap.ini &
 DEMO_PID=$!
-EXIT=$(($EXIT+$?))
-sleep 15 
-echo "Started server in PID"
-echo $DEMO_PID
+
+trap 'echo "trying to kill $DEMO_PID" && kill -2 $DEMO_PID && echo "killed $DEMO_PID" && wait $DEMO_PID && echo "waited for $DEMO_PID";' INT
+sleep 10
+
 [ $(curl -i http://localhost:8000/docs.json 2>/dev/null | head -n 1 | cut -d$' ' -f2) == "200" ]
 EXIT=$(($EXIT+$?))
 [ $(curl -i http://localhost:8000/20/topics.json 2>/dev/null | head -n 1 | cut -d$' ' -f2) == "200" ]
@@ -26,25 +27,17 @@ EXIT=$(($EXIT+$?))
 EXIT=$(($EXIT+$?))
 [ $(curl -i http://localhost:8000/20/ 2>/dev/null | head -n 1 | cut -d$' ' -f2) == "200" ]
 EXIT=$(($EXIT+$?))
-[ $(curl -i http://localhost:8000/topics/ 2>/dev/null | head -n 1 | cut -d$' ' -f2) == "200" ]
+[ $(curl -i http://localhost:8000/topics 2>/dev/null | head -n 1 | cut -d$' ' -f2) == "200" ]
 EXIT=$(($EXIT+$?))
 
-wait $DEMO_PID
-echo "killing server in PID"
-echo $DEMO_PID
-kill -INT $DEMO_PID
-wait $DEMO_PID
-trap - TERM INT
+kill -2 $$
+trap - INT
 
-
-#$CMD -m topicexplorer.train ap.ini --rebuild -k 20 40 60 --iter 20 --context-type article
-#EXIT=$(($EXIT+$?))
+$CMD -m topicexplorer.train ap.ini --rebuild -k 20 40 60 --iter 20 --context-type article
+EXIT=$(($EXIT+$?))
 $CMD -m topicexplorer update
 # TODO: enable once status code for invalid branch is implemented
 # EXIT=$EXIT+$?
-
-
-
 
 coverage report
 echo "Exiting with code ${EXIT}"
