71259ba020ce28bb42604d1d0265d01a7ce73a2b
Jaimie Murdock <jaimie.murdock@gmail.com>
2017-10-03 10:14:49 -0400

    vesion bump for a variety of interface fixes

--- topicexplorer/version.py
+++ topicexplorer/version.py
@@ -7,7 +7,7 @@ class _VersionModule(types.ModuleType):
 
     @property
     def __version__(self):
-        return "1.0b170"
+        return "1.0b171"
 
     @property
     def __pretty_version__(self):
