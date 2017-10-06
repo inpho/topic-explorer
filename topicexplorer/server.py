ee73476119137742aa629161da2aae2c61836d1f
Jaimie Murdock <jaimie.murdock@gmail.com>
2017-10-06 10:53:41 -0400

    removing decode/encode errors

--- topicexplorer/server.py
+++ topicexplorer/server.py
@@ -486,7 +486,6 @@ class Application(Bottle):
         @self.route('/fulltext/<doc_id:path>')
         @_set_acao_headers
         def get_doc(doc_id):
-            doc_id = doc_id.decode('utf-8')
             pdf_path = os.path.join(corpus_path, re.sub('txt$', 'pdf', doc_id))
             if os.path.exists(pdf_path.encode('utf-8')):
                 doc_id = re.sub('txt$', 'pdf', doc_id)
@@ -494,9 +493,7 @@ class Application(Bottle):
                 doc_id = doc_id + '.txt'
             # here we deal with case where corpus_path and doc_id overlap
             (fdirs, lastdir) = os.path.split(corpus_path)
-            pattern = lastdir.decode('utf-8')
-            fdirs = fdirs.decode('utf-8')
-            if re.match('^' + pattern, doc_id):
+            if re.match('^' + lastdir, doc_id):
                 return static_file(doc_id, root=fdirs)
             else:
                 return static_file(doc_id, root=corpus_path)
