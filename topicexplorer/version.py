949ac04a497362f342d8c12d285b4d9259319f48
Jaimie Murdock <jaimie.murdock@gmail.com>
2017-12-04 16:13:25 -0500

    version bump for waitress server

--- topicexplorer/version.py
+++ topicexplorer/version.py
@@ -9,7 +9,7 @@ class _VersionModule(types.ModuleType):
 
     @property
     def __version__(self):
-        return "1.0b189"
+        return "1.0b190"
 
     @property
     def __pretty_version__(self):
