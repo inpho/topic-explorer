14ea2aacc2e7a475d737c30db5cc4ef0bed0d416
Jaimie Murdock <jaimie.murdock@gmail.com>
2018-02-18 11:02:29 -0500

    version bump for dependnency fixes

--- topicexplorer/version.py
+++ topicexplorer/version.py
@@ -9,7 +9,7 @@ class _VersionModule(types.ModuleType):
 
     @property
     def __version__(self):
-        return "1.0b195"
+        return "1.0b196"
 
     @property
     def __pretty_version__(self):
