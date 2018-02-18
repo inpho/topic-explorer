4559b66bcbf541a4abf8b5fb43596fa84e030c24
Jaimie Murdock <jaimie.murdock@gmail.com>
2018-02-18 18:06:35 -0500

    removing the install_data script - just do it all at runtime. closes #252

--- setup.py
+++ setup.py
@@ -1,26 +1,11 @@
 #!/usr/bin/env python
 
-from distutils.command.install_data import install_data as _install_data
-from setuptools import setup, find_packages
-import os
-import platform
+from setuptools import setup
 import os.path
 
 # get version from package through manual read
 # see http://stackoverflow.com/a/17626524
 __version__ = open(os.path.normpath("topicexplorer/version.py")).readlines()[11].split()[-1].strip("\"'")
-
-# Specializations of some distutils command classes
-# first install data files to actual library directory
-class PostInstallData(_install_data):
-    """need to change self.install_dir to the actual library dir"""
-    def run(self):
-        import nltk
-        runcmd = _install.run(self)
-        nltk.download('punkt')
-        nltk.download('stopwords')
-        nltk.download('wordnet')
-        return runcmd
 
 # PyPandoc
 if os.path.exists('README.txt'):
@@ -33,7 +18,6 @@ setup(
     setup_requires=['pbr', 'nltk'],
     version=__version__,
     long_description = long_description,
-    cmdclass={'install_data' : PostInstallData},
     test_suite="unittest2.collector",
     tests_require=['unittest2', 'mock']
 )
