19efc8b8df6d0c2261b75b06ee087807c2cd3a86
Jaimie Murdock <jaimie.murdock@gmail.com>
2018-02-19 19:06:48 -0500

    more verbose reporting

--- coverage.ps1
+++ coverage.ps1
@@ -1,9 +1,11 @@
 function Test-Url($url, $response) {
     Try{
-        (Invoke-WebRequest $url | Select-Object -ExpandProperty StatusCode) -eq $response
+        $resp = (Invoke-WebRequest $url | Select-Object -ExpandProperty StatusCode)
     } Catch {
-        $_.Exception.Response.StatusCode.value__ -eq $response
+        $resp = $_.Exception.Response.StatusCode.value__
     }
+    Write-Host ($url,"(Expected:",$response,"Returned:",$resp,")") -Separator " "
+    $resp -eq $response
 }
 
 Test-Url "http://localhost:8000/" 200
@@ -14,9 +16,9 @@ Test-Url "http://localhost:8000/docs.json?random=1" 200
 Test-Url "http://localhost:8000/docs.json?q=bush" 200
 Test-Url "http://localhost:8000/docs.json?id=AP900817-0118" 200
 Test-Url "http://localhost:8000/20/topics.json" 200
-Test-Url "http://localhost:8000/topics.json?q=bush|israel"" 200
-Test-Url "http://localhost:8000/topics.json?q=foobar"" 404
-Test-Url "http://localhost:8000/topics.json?q=a|the"" 410
+Test-Url "http://localhost:8000/topics.json?q=bush|israel" 200
+Test-Url "http://localhost:8000/topics.json?q=foobar" 404
+Test-Url "http://localhost:8000/topics.json?q=a|the" 410
 Test-Url "http://localhost:8000/20/topics/1.json" 200
 Test-Url "http://localhost:8000/20/topics/1.json?n=-20" 200
 Test-Url "http://localhost:8000/topics.json?q=bush" 200
