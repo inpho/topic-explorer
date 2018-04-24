f3b37a3c95ff375649f043a400ebbb5962c28e3c
Jaimie Murdock <jaimie.murdock@gmail.com>
2018-04-23 21:14:50 -0400

    version bumps for vsm and topics

--- topicexplorer/version.py
+++ topicexplorer/version.py
@@ -9,7 +9,7 @@ class _VersionModule(types.ModuleType):
 
     @property
     def __version__(self):
-        return "1.0b203"
+        return "1.0b204"
 
     @property
     def __pretty_version__(self):
