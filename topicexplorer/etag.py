ea621c22c8a5b05aa7c638f504af3329b4a85234
Jaimie Murdock <jaimie.murdock@gmail.com>
2018-04-26 14:16:17 -0400

    cleanup of etag sketch

--- topicexplorer/etag.py
+++ topicexplorer/etag.py
@@ -1,15 +1,12 @@
 from corpus import *
 import hashlib
-from bottle import (abort, redirect, request, response, route, run,
-                    static_file, Bottle, ServerAdapter)
-import numpy as np
 
 def generate_etag(v):
     ''' Takes a model view and generates an etag using the v.phi and v.theta attributes '''
     # TODO: write a function using a hashlib digest
     x = hashlib.sha1()
-    x.update(repr(self.v[k].phi).encode('utf-8))
-    x.update(repr(self.v[k].theta).encode('utf-8))
+    x.update(repr(v.phi).encode('utf-8'))
+    x.update(repr(v.theta).encode('utf-8'))
     return x.hexdigest()
 
 
