5ec36b724e84c31e6c51618435ff687623487a7d
Jaimie Murdock <jaimie.murdock@gmail.com>
2018-05-19 19:39:05 -0400

    version bump for prep fixes

--- topicexplorer/version.py
+++ topicexplorer/version.py
@@ -9,7 +9,7 @@ class _VersionModule(types.ModuleType):
 
     @property
     def __version__(self):
-        return "1.0b209"
+        return "1.0b210"
 
     @property
     def __pretty_version__(self):
