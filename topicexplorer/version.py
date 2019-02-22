6ef57591b799176a259895be04b5950b4fd3bbba
Jaimie Murdock <jaimie.murdock@gmail.com>
2019-02-22 16:42:54 -0500

    version bump for htrc fixes

--- topicexplorer/version.py
+++ topicexplorer/version.py
@@ -9,7 +9,7 @@ class _VersionModule(types.ModuleType):
 
     @property
     def __version__(self):
-        return "1.0b221"
+        return "1.0b222"
 
     @property
     def __pretty_version__(self):
